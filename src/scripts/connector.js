da006bafaf5f1de8d663cc5adff7f4e68840767d
an_bir <an_bir@mail.com>
2022-10-10 14:17:40 +0300

    modified

--- src/scripts/connector.js
+++ src/scripts/connector.js
@@ -2,12 +2,10 @@
 console.log(123123);
 
 window.TrelloPowerUp.initialize({
-  "card-badges": (t, options) => {
-    return t.card("all").then((card) => {
-      console.log(card)
-      return [{
-        text: card.idShort
-      }]
-    })
-  }
-})
+  'card-buttons': function(t, options){
+    return [{
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Estimate Size',
+    }];
+  },
+});
