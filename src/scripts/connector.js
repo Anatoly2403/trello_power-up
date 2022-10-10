00186c7454b0f4a0d6028b591cb5f6e05bd981d7
an_bir <an_bir@mail.com>
2022-10-10 17:30:53 +0300

    modified

--- src/scripts/connector.js
+++ src/scripts/connector.js
@@ -12,6 +12,7 @@ window.TrelloPowerUp.initialize({
   },
   'card-badges': function (t, opts) {
     console.log(t)
+    console.log(t.getContext())
     return [{
       text: 'No Estimate!',
       color: 'blue',
